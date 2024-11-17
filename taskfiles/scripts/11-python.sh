bc814e11c0066a8fcc4a0339d51f78a7de684ec0
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-11-17 14:18:40 +0100

    fix: correct python installation script

--- taskfiles/scripts/11-python.sh
+++ taskfiles/scripts/11-python.sh
@@ -8,14 +8,13 @@ PKGS=(
   pyenv
   python-pip
   python-pipx
-  python-pynvim
 )
 
 echo "Installing required packages for development"
 
 for PKG in "${PKGS[@]}"; do
   echo "Installing package: $PKG"
-  yay -S "$PKG" --noconfirm --needed
+  sudo pacman -S "$PKG" --noconfirm --needed
 done
 
 echo "Development setup is complete"
