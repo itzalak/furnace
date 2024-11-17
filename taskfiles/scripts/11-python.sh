6c14921e7d3777cb529f7adebeaa0a4e870a2a13
Itzalak <24299428+itzalak@users.noreply.github.com>
2024-11-17 12:54:12 +0100

    chore: add submodules taskfile

--- taskfiles/scripts/11-python.sh
+++ taskfiles/scripts/11-python.sh
@@ -2,16 +2,6 @@
 
 set -uo pipefail
 
-# Define some colors for output
-YELLOW=$(tput setaf 3)
-RESET=$(tput sgr0)
-
-# Print an info message
-function print_info() {
-  echo
-  echo -e "${YELLOW}$1${RESET}"
-}
-
 PKGS=(
   # Python
   python
@@ -21,11 +11,11 @@ PKGS=(
   python-pynvim
 )
 
-print_info "Installing required packages for development"
+echo "Installing required packages for development"
 
 for PKG in "${PKGS[@]}"; do
-  print_info "Installing package: $PKG"
+  echo "Installing package: $PKG"
   yay -S "$PKG" --noconfirm --needed
 done
 
-print_info "Development setup is complete"
+echo "Development setup is complete"
